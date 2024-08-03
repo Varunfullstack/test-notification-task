a545f4ba157ff480424bc31f2d4cbb5a50551b63
Varun <varunphp.dev@gmail.com>
2024-08-03 15:10:06 +0530

    feat: firebase emulator setup

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,18 +1,13 @@
-// firebaseConfig.ts
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-// Firebase configuration object
-const firebaseConfig = {
-  apiKey: "AIzaSyCoV4f4hlDs8q8jpWiBYB-Qfp_l4FSsyQw",
-  authDomain: "react-notify-1d5fe.firebaseapp.com",
-  projectId: "react-notify-1d5fe",
-  storageBucket: "react-notify-1d5fe.appspot.com",
-  messagingSenderId: "941198286065",
-  appId: "1:941198286065:web:15ed89d8ba1bed36f19d50",
-  measurementId: "G-BLN73CZ7S0",
-};
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const firebase_db = getFirestore(app);
+const app = initializeApp({ projectId: "react-notify-1d5fe" });
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Connect to Firestore emulator in local development
+connectFirestoreEmulator(db, "localhost", 8080);
+
+export const firebase_db = db;
